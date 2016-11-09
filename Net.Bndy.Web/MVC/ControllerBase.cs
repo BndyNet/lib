bbaac1999146ff1a18ea3ac93d36dcb6201da6eb
Bndy <zb@bndy.net>
2016-11-09 20:52:52 +0800

    about mvc

---
+++ Net.Bndy.Web/MVC/ControllerBase.cs
@@ -0,0 +1,36 @@
+﻿// =================================================================================
+// Copyright (c) 2016 http://www.bndy.net.
+// Created by Bndy at 11/9/2015 7:55:32 PM
+// ---------------------------------------------------------------------------------
+// Summary
+// =================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Net.Bndy.Web.MVC
+{
+    public class ControllerBase : Controller
+    {
+        /// <summary>
+        ///  Creates a JsonNetResult object that serializes the specified object
+        ///     to JavaScript Object Notation (JSON) format using the specified content type
+        ///     and JSON request behavior.
+        /// </summary>
+        /// <param name="data">The JavaScript object graph to serialize.</param>
+        /// <param name="behavior"> The JSON request behavior.</param>
+        /// <returns>The result object that serializes the specified object to JSON format. </returns>
+        protected JsonNetResult JsonNet(object data, JsonRequestBehavior behavior = JsonRequestBehavior.AllowGet)
+        {
+            return new JsonNetResult()
+            {
+                Data = data,
+                JsonRequestBehavior = behavior,
+            };
+        }
+    }
+}
---
+++ Net.Bndy.Web/MVC/JsonNetResult.cs
@@ -0,0 +1,43 @@
+﻿// =================================================================================
+// Copyright (c) 2016 http://www.bndy.net.
+// Created by Bndy at 11/9/2015 7:38:14 PM
+// ---------------------------------------------------------------------------------
+// Summary
+// =================================================================================
+
+using Newtonsoft.Json;
+using System;
+using System.Web.Mvc;
+
+namespace Net.Bndy.Web.MVC
+{
+    public class JsonNetResult : JsonResult
+    {
+        /// <summary>
+        /// Enables processing of the result of an action method by a custom type that inherits from the <see cref="T:System.Web.Mvc.ActionResult" /> class.
+        /// </summary>
+        /// <param name="context">The context within which the result is executed.</param>
+        /// <exception cref="ArgumentNullException">context</exception>
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            var response = context.HttpContext.Response;
+
+            response.ContentType = !String.IsNullOrEmpty(ContentType)
+                ? ContentType
+                : "application/json";
+
+            if (ContentEncoding != null)
+                response.ContentEncoding = ContentEncoding;
+
+            var serializedObject = JsonConvert.SerializeObject(Data, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            });
+
+            response.Write(serializedObject);
+        }
+    }
+}
