e516ea872b83381b8216608ea823d205a89af0b5
Bndy <zb@bndy.net>
2016-11-10 22:08:15 +0800

    Init  a project about log

---
+++ Net.Bndy.Log/Logger.cs
@@ -0,0 +1,22 @@
+﻿// =================================================================================
+// Copyright (c) 2016 http://www.bndy.net.
+// Created by Bndy at 11/10/2016 9:27:19 PM
+// ---------------------------------------------------------------------------------
+// About Logs
+// =================================================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Net.Bndy.Log
+{
+    public class Logger
+    {
+        public static void Init()
+        {
+            //log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
